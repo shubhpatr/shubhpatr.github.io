72ac9f23ad4c6fcf87c2f5df7e8d98e68bd28e15
Shubham Mohapatra <shubham.mohapatra1996@gmail.com>
2021-11-19 17:43:36 -0500

    added a new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -12,11 +12,11 @@ export const projectsData = [
     {
         id: 1,
         projectName: 'Map Reduce Search Engine',
-        projectDesc: 'Application that triggers map reduce jobs on 250+ files on prject guetenberg',
+        projectDesc: 'Application that triggers inverted index jobs on the entire project guetenberg repository',
         tags: ['AppEngine', 'CloudShell','Python'],
         code: 'https://fa21ecc40249shubham.uc.r.appspot.com/',
         demo: 'https://github.com/shubhpatr/scalable-mapr',
-        image: 'https://cdn.searchenginejournal.com/wp-content/uploads/2021/08/great-search-engines-612f8f78d1f79-sej-1280x720.jpg'
+        image: 'https://cdn.pixabay.com/photo/2016/03/04/19/36/gears-1236578__340.jpg'
     },
     {
         id: 2,
